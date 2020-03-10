d8ee697cb9210d2a944813ca241301ce35499c79
Nicole <nicole.92x@live.com>
2020-03-10 14:08:34 +0000

    Updated tests for take_purchase
    verifying product attributes match order attributes.

--- tests/test_take_purchase.py
+++ tests/test_take_purchase.py
@@ -6,7 +6,10 @@ from products import Product
 class TakePurchase(TestCase):
     def test_take_purchase(self):
         product = Product("alice in wonderland", Decimal("10.00"), "children's book")
-        purchase = take_purchase(product, 2)
-        self.assertEqual(purchase.product_name, "alice in wonderland")
-        self.assertEqual(purchase.cost, Decimal("10.00"))
-        self.assertEqual(purchase.qty, 2)
+        quantity = 2
+
+        purchase = take_purchase(product, quantity)
+
+        self.assertEqual(purchase.product_name, product.name)
+        self.assertEqual(purchase.cost, product.cost)
+        self.assertEqual(purchase.qty, quantity)
