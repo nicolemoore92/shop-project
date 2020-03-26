8a0e0417ca95fdd8544c499396b346e003068cf6
Nicole <nicole.92x@live.com>
2020-03-26 11:04:08 +0000

    Optimised code for performance

--- display_products.py
+++ display_products.py
@@ -2,7 +2,7 @@ from products import Product
 
 def display_products(products):
     """Display many products function"""
-    d_products = "Name: Cost: "
+    d_products = ["Name: Cost: "]
     for product in products:
-        d_products += f"\n {product.name.title()} £{product.cost}"
-    return d_products
+        d_products.append(f"\n {product.name.title()} £{product.cost}")
+    return "".join(d_products)
