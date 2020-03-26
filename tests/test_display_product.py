084f22ac58bf37ea23ec94447e4fa343cd2dfcd7
Nicole <nicole.92x@live.com>
2020-03-26 14:36:36 +0000

    Updated test for display_product

--- tests/test_display_product.py
+++ tests/test_display_product.py
@@ -6,9 +6,6 @@ from display_product import display_product
 class TestDisplayProduct(TestCase):
     def test_display_product(self):
         product = Product("alice in wonderland", Decimal("10.00"), "children's book")
-        self.assertEqual(product.name.title(), "Alice In Wonderland")
-        self.assertEqual(product.cost, Decimal("10.00"))
-
         display = display_product(product)
         expected_display = "Name: Cost: \nAlice In Wonderland £10.00"
         self.assertEqual(display, expected_display)
