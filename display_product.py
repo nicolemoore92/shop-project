089495c1643ce5be444db02afbd8dbd188fe4113
Nicole <nicole.92x@live.com>
2020-03-26 14:43:00 +0000

    Removed test data

--- display_product.py
+++ display_product.py
@@ -4,7 +4,3 @@ from decimal import Decimal
 def display_product(product):
     """Display a product function"""
     return f"Name: Cost: \n{product.name.title()} £{product.cost}"
-
-product = Product("alice in wonderland", Decimal("10.00"), "children's book")
-
-print(display_product(product))
