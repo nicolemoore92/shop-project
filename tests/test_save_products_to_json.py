443d4a3551d477d1357c6eb5b3b702cb3faeee05
Nicole <nicole.92x@live.com>
2020-03-30 16:34:23 +0100

    Add test for dump_products_to_json

--- tests/test_save_products_to_json.py
+++ tests/test_save_products_to_json.py
@@ -4,6 +4,7 @@ import json
 from decimal import Decimal
 from save_products_to_json import save_products_to_json
 from textwrap import dedent
+from save_products_to_json import dump_products_to_json
 
 class SaveProductsToJson(TestCase):
     def test_save_products_to_json(self):
@@ -28,3 +29,25 @@ class SaveProductsToJson(TestCase):
 
         with open("product_data.json") as pdata:
             self.assertEqual(json.load(pdata), expected_data)
+
+    def test_dump_products_to_json(self):
+        product1 = Product("alice in wonderland", Decimal("10.00"), "children's book")
+        product2 = Product("the hobbit", Decimal("9.99"), "fantasy adventure")
+        product_list = [product1, product2]
+
+        pdata = dump_products_to_json(product_list)
+
+        expected_data = [
+         {
+          "name": "alice in wonderland",
+          "cost": "10.00",
+          "description": "children's book"
+         },
+         {
+          "name": "the hobbit",
+          "cost": "9.99",
+          "description": "fantasy adventure"
+         }
+        ]
+
+        self.assertEqual(json.loads(pdata), expected_data)
