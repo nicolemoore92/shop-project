57e994bf735715989275c614e2d7a918c4f820e5
Nicole <nicole.92x@live.com>
2020-03-30 16:03:15 +0100

    Split save_products_to_json into 2 functions
    So we can produce product data with the option to save to a file or work
    with a Python string.

--- save_products_to_json.py
+++ save_products_to_json.py
@@ -1,9 +1,13 @@
 import json
 
 def save_products_to_json(products, filename = 'product_data.json'):
-      product_dicts = []
-      for product in products:
-          product_dict = {'name': product.name, 'cost': str(product.cost), 'description': product.description}
-          product_dicts.append(product_dict)
-      with open(filename, 'w') as pd:
-          json.dump(product_dicts,pd, indent=1)
+    output = dump_products_to_json(products)
+    with open(filename, 'w') as pd:
+        pd.write(output)
+
+def dump_products_to_json(products):
+    product_dicts = []
+    for product in products:
+        product_dict = {'name': product.name, 'cost': str(product.cost), 'description': product.description}
+        product_dicts.append(product_dict)
+    return json.dumps(product_dicts, indent=1)
